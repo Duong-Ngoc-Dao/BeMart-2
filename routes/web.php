e1c8b1026b4a6faa1caacc2fcc9806273a111811
Huy2003zzz <huy072806@gmail.com>
2024-02-26 21:02:51 +0700

    router

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\Admin\RoleController;
+use App\Http\Controllers\Admin\SupplierController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\DashboadContrller;
+use App\Http\Controllers\Admin\GroupUserController;
+use App\Http\Controllers\Admin\CustomerController;
+use App\Http\Controllers\Admin\ConfigController;
+use App\Http\Controllers\Admin\CategoriBlogController;
+use App\Http\Controllers\Admin\ContactContrller;
+use App\Http\Controllers\Admin\OrderContrller;
+use App\Http\Controllers\Admin\BlogsController;
+use App\Http\Controllers\Admin\SaleController;
+use App\Http\Controllers\Admin\SessionController as AdminSessionController;
+use App\Http\Controllers\RegisteredStoreController;
+use App\Http\Controllers\Client\ClientController;
 
 /*
 |--------------------------------------------------------------------------
