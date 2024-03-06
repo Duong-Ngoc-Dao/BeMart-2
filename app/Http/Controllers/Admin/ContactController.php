5d08fd6e16eeebc4e627bc3c6d7580a74334b7f0
phiphung <phungvietphi88@gmail.com>
2024-03-06 16:02:07 +0700

    Order + Contact + Role Controller

--- app/Http/Controllers/Admin/ContactController.php
+++ app/Http/Controllers/Admin/ContactController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\contact;
-class ContactContrller extends Controller
+class ContactController extends Controller
 {
     public function index(Request $request)
     {
---
+++ app/Http/Controllers/Admin/OrderController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\OrderDetail;
+use App\Models\Order;
+class OrderController extends Controller
+{
+    public function index(Request $request)
+    {
+
+        $Orders = OrderDetail::where('users_id', auth()->user()->id)->orderBy('id', 'DESC')->Paginate(30);
+        return view('admin.pages.orders.index', compact('Orders'));
+    }
+
+    public function edit($id)
+    {
+        $Orders = Order::where('id', $id)->first();
+        $Orders->load('order_detail');
+        if(!$Orders){
+            return redirect()->back();
+        }
+        if($Orders){
+            //  dd($Orders);
+              $totalMoney = 0;
+              foreach ($Orders->order_detail as $order) {
+                  $totalMoney += $order->price * $order->quantity;
+              }
+            return view('admin.pages.orders.edit', compact('Orders','totalMoney'));
+            }
+            return redirect()->back()->with('error', 'Không tìm thấy sản phẩn của đơn hàng');
+    }
+
+    public function update(Request $request ,$id)
+    {
+        $this->validate(request(),[
+            'status' => 'required|integer|min:0',
+        ],
+        [
+            'status.required' => 'Trạng thái Không hợp lệ',
+            'status.integer' => 'Trạng thái Không hợp lệ',
+            'status.min'=>'Trạng thái Không hợp lệ',
+        ]);
+        $Orders = OrderDetail::where('id', $id)->first();
+        if(!$Orders){
+            return redirect()->back();
+        }
+        $Orders->update($request->all());
+        return response()->json([
+            'message' => "Cập nhật trạng thái thành công",
+            'status' => "200"
+        ]);
+    }
+}
