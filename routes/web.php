8d730bae10101d6b5f78bf919683f34d9eea9ef7
Huy2003zzz <huy072806@gmail.com>
2024-03-08 00:32:20 +0700

    router

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,4 @@ Route::get('cp-register', [ClientController::class, 'register'])->name('register
 Route::post('cp-register', [ClientController::class, 'registerCreate'])->name('registerCreate');
 Route::get('quen-mat-khau', [ClientController::class, 'forgetPassword'])->name('forgetPassword');
 Route::post('quen-mat-khau', [ClientController::class, 'SentPassword'])->name('SentPassword');
+Route::get('doi-mat-khau', [ClientController::class, 'ChangePassword'])->name('ChangePassword');
