382f1223451e12de1063a9db83a53762583bd0d6
Huy2003zzz <huy072806@gmail.com>
2024-03-22 22:09:06 +0700

    router

--- routes/web.php
+++ routes/web.php
@@ -53,5 +53,6 @@ Route::name('cp-admin.')->middleware('AdminLogin')->prefix('cp-admin/')->group(f
         Route::get('profile', [UserController::class, 'proFile'])->name('profile');
         Route::post('profile', [UserController::class, 'proFileStore'])->name('proFileStore');
         Route::post('changePassword', [UserController::class, 'changePassword'])->name('changePassword');
+        Route::get('config', [ConfigController::class, 'config'])->name('config');
     });
  });  
