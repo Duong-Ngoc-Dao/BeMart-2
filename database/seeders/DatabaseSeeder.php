2fa802d70a870aff64525c599dfa542f0610f9ae
dao <dao24252003@gmail.com>
2024-03-29 20:28:06 +0700

    sd-6

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\BlogSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +15,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(RoleSeed::class);
+        $this->call(PermissionSeed::class);
+        $this->call(PermissionRoleSeed::class);
+        $this->call(ConfigSeeder::class);
+        $this->call(GroupUserSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(SupplierSeeder::class);
+        $this->call(OriginSeeder::class);
+        $this->call(CategorySeeder::class);
+        // $this->call(SaleSeeder::class);
+        $this->call(ProductSeeder::class);
+        $this->call(OrderSeeder::class);
+        $this->call(CateBlogSeeder::class);
+        $this->call(BlogSeeder::class);
     }
 }
